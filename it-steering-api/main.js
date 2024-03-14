6797101cf67328aa6b1a18866600121872138214
amar <amar.ibrahimovic7@gmail.com>
2024-03-14 15:49:01 +0100

    API endpoints barebones

--- it-steering-api/main.js
+++ it-steering-api/main.js
@@ -15,3 +15,36 @@ app.listen(port, ()=>{
 app.get('/', (req, res)=>{
     res.send('You can access the IT Steering API, and it works');
 });
+
+app.get('/projects', (req, res)=>{
+    res.send('GET all projects');
+});
+
+app.post('/projects', (req, res)=>{
+    res.send('POST new project');
+});
+
+app.delete('/projects/:projectId', (req, res)=>{
+    res.send('DELETE project');
+});
+
+app.put('/projects/:projectId', (req, res)=>{
+    res.send('PUT project');
+});
+
+app.post('/login', (req, res)=>{
+    res.send('POST log in');
+});
+
+app.post('/logout', (req, res)=>{
+    res.send('POST log out');
+});
+
+/*
+1. GET all projects
+2. POST new project
+3. DELETE project
+4. PUT project 
+5. POST log in
+6. POST Log out
+*/
