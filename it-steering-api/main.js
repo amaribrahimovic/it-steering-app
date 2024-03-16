00db66a15d53e844a1cf663018c3f4ce26fe8397
amar <amar.ibrahimovic7@gmail.com>
2024-03-16 04:35:55 +0100

    Fixed cors and cookie issues

--- it-steering-api/main.js
+++ it-steering-api/main.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {dbName: 'it-steering-ap
 
 // Main Setup
 const app = express();
-app.use(cors());
+app.use(cors({origin: 'http://localhost:3000', credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -54,34 +54,26 @@ const checkJWT = req => {
 }
 
 // Api Calls
-app.get('/projects', async (req, res)=>{
-    try{
-        if(await checkJWT(req))
-            res.send(await Project.find());
-        else
-            res.status(401).json({ message: 'You are not logged in'});
-    } catch(err){
-        console.error(err);
-        res.status(500).send('An error has occurred while fetching all projects');
-    }
-});
 
+// Check if user is logged in
 app.get('/user', async (req, res)=>{
-    const token = req.cookies.jwt
-    if (token) {
-        jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
-            if (err) 
-                res.status(401).json({ message: 'You are not logged in' });
-            else{
-                const user = await User.findById(decodedToken.id);
-                res.status(200).json({ message: `${user}` });
-            }
-        });
-    } else{
-        res.status(401).json({ message: 'You are not logged in' });
+    try{
+        const token = req.cookies.jwt;
+        if (token) {
+            jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
+                if (!err){
+                    const user = await User.findById(decodedToken.id);
+                    res.status(200).json(user);
+                }  
+            });
+        } 
+    } catch(err){
+        console.error(err)
     }
+    
 });
 
+// Get all of user's projects
 app.get('/user/projects/:userId', async (req, res)=>{
     try{
         if(await checkJWT(req)){
@@ -95,19 +87,37 @@ app.get('/user/projects/:userId', async (req, res)=>{
     }
 });
 
-app.post('/projects', async (req, res)=>{
+// Get all projects
+app.get('/projects', async (req, res)=>{
+    try{
+        if(await checkJWT(req))
+            res.send(await Project.find());
+        else
+            res.status(401).json({ message: 'You are not logged in'});
+    } catch(err){
+        console.error(err);
+        res.status(500).send('An error has occurred while fetching all projects');
+    }
+});
+
+// Create a project and add it to user's array of projects
+app.post('/projects/:userId', async (req, res)=>{
     try{
         if(await checkJWT(req)){
-            await Project.create(req.body);
-            res.status(200).send('A project has been created');
-        }
-        res.status(401).json({ message: 'You are not logged in'});
+            const newProject = await Project.create(req.body);
+            const user = await User.findById(req.params.userId).populate('projects');
+            user.projects.push(newProject);
+            await user.save()
+            res.status(200).json({ message: 'A project has been created' });
+        } else
+            res.status(401).json({ message: 'You are not logged in'});
     } catch(err){
         console.error(err);
         res.status(500).send('An error has occurred while creating a project');
     }
 });
 
+// Delete a project
 app.delete('/projects/:projectId', async (req, res)=>{
     try{
         if(await checkJWT(req)){
@@ -121,6 +131,7 @@ app.delete('/projects/:projectId', async (req, res)=>{
     }
 });
 
+// Update a project
 app.put('/projects', async (req, res)=>{
     try{
         if(await checkJWT(req)){
@@ -136,9 +147,10 @@ app.put('/projects', async (req, res)=>{
     }
 });
 
+// Create a user account
 app.post('/signup', async (req, res)=>{
     try{
-        const user = await User.create(req.body);
+        const user = await User.create({username: req.body.username, password: req.body.password, admin: false, projects: []});
         const token = generateJWT(user._id);
         res.cookie('jwt', token, { maxAge: 259200000});
         res.status(200).json({ user: user._id });
@@ -148,6 +160,7 @@ app.post('/signup', async (req, res)=>{
     }
 });
 
+// Login with a user account
 app.post('/login', async (req, res)=>{
     try{
         const { username, password } = req.body;
@@ -158,7 +171,7 @@ app.post('/login', async (req, res)=>{
         }
 
         const token = generateJWT(user._id);
-        res.cookie('jwt', token, { maxAge: 259200000});
+        res.cookie('jwt', token, { maxAge: 259200000 });
         res.status(200).json({ message: 'You have been logged in', user: user._id });
     } catch(err){
         console.error(err);
@@ -166,6 +179,7 @@ app.post('/login', async (req, res)=>{
     }
 });
 
+// Logout 
 app.get('/logout', (req, res)=>{
     res.cookie('jwt', '', { maxAge: 1 });
     res.status(200).json({ message: 'Logged out'});
